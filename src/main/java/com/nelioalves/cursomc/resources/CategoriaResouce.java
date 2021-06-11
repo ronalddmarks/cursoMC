ab18091ff7ee2eefa7c5161356aa27b2da528f2a
D1MJ <ronaldmarques.spassu@petrobras.com.br>
2021-06-10 21:24:11 -0300

    Criando repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResouce.java
@@ -1,32 +1,29 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.categoriaService;
 
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private categoriaService service;
+	
+	@RequestMapping(value="/{id}",  method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
+		Categoria obj = service.buscar(id);
 		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
